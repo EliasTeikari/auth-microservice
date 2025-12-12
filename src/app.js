30c1aa834f19cf6821553b648dbe1a9674dfa02c
Elias Mikael Teikari <EliasTeikari@Margits-iPro-2.lan>
2025-12-12 23:25:01 +0200

    Setup mongoose schema and model

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
 const login = require("./routes/login");
-require("dotenv").config();
 const port = process.env.PORT | 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(express.json());
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
-app.get("/users", (req, res) => {
-    res.send("Hello world user");
-});
+const user = new Sche();
 
 app.listen(port, () => {
     console.log("App listening on port 3000");
--- src/routes/login.js
+++ src/routes/login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
+app.use(router);
 app.router("/login");
 
 router.get("/", (req, res) => {});
